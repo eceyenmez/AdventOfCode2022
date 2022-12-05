075cc848f685dae9e5b81d8369181dd4bfdbf409
Ece İlayda Yenmez <eceyenmez@gmail.com>
2022-12-05 21:25:39 +0100

    Solved day 1 second part with elixir

--- Day_1/CalorieCounting.ex
+++ Day_1/CalorieCounting.ex
@@ -2,7 +2,7 @@ defmodule CalorieCounting do
 
   def count_calories do
 
-    content = File.stream!("input.txt")
+    File.stream!("input.txt")
     |> Enum.map(fn x ->
       case x do
         "\n" -> "-"
@@ -12,6 +12,24 @@ defmodule CalorieCounting do
     |> Enum.chunk_by(&(&1 == "-"))
     |> Enum.filter(fn x -> x != ["-"] end)
     |> Enum.reduce(0, fn x, acc -> if (sum = Enum.sum(x)) > acc, do: sum, else: acc end)
+
+  end
+
+  def count_calories_2 do
+
+    File.stream!("input.txt")
+    |> Enum.map(fn x ->
+      case x do
+        "\n" -> "-"
+        _ -> x |> String.trim |> String.to_integer
+        end
+      end)
+    |> Enum.chunk_by(&(&1 == "-"))
+    |> Enum.filter(fn x -> x != ["-"] end)
+    |> Enum.map(fn x -> Enum.sum(x) end)
+    |> Enum.sort(&(&1 >= &2))
+    |> Enum.take(3)
+    |> Enum.sum
 
   end
 
