9e81a1b8a5d8ed9d8d4b0bfeef57573b80802b03
Ece İlayda Yenmez <eceyenmez@gmail.com>
2022-12-11 20:37:33 +0100

    Solved day 3 question 2

--- Day_3/rucksack_reorganisation.ex
+++ Day_3/rucksack_reorganisation.ex
@@ -19,12 +19,36 @@ defmodule RucksackReorganisation do
 
   end
 
+  def organise_rucksack_2 do
+
+      File.stream!("input.txt")
+      |> Enum.map(&(String.trim(&1)))
+      |> Enum.chunk_every(3)
+      |> Enum.reduce(0,fn list, sum ->
+        value = get_intersecting_character(list)
+        |> MapSet.to_list
+        |> List.to_string
+        |> :binary.first
+        |> get_character_value
+
+        sum + value
+      end)
+  end
+
   defp get_character_value(char) when char <= 90 do
     char - 38
   end
 
   defp get_character_value(char) do
     char - 96
+  end
+
+  defp get_intersecting_character(list) do
+    [first,second,third] = Enum.map(list, fn x -> Enum.into(String.graphemes(x), MapSet.new) end)
+
+    first
+    |>MapSet.intersection(second)
+    |>MapSet.intersection(third)
   end
   
 end
